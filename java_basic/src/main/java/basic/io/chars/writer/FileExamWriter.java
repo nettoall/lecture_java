cdff7ebc0f696d04971dbe98c73772a479cd10e8
nettoall <nettoall@gmail.com>
2022-04-15 23:54:17 +0900

    2022.04.15 소스 변경 분 반영

---
+++ java_basic/src/main/java/basic/io/chars/writer/FileExamWriter.java
@@ -0,0 +1,11 @@
+package basic.io.chars.writer;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileExamWriter {
+    public static void main(String[] args) throws IOException {
+        FileWriter fw = new FileWriter("./output.txt"   );
+
+    }
+}
