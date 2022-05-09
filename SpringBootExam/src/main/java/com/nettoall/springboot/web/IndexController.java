43ba0ec4e44126e2b1c30d32adcf4ed09d3dd208
nettoall <nettoall@gmail.com>
2022-05-09 13:48:12 +0900

    2022.05.09 spring boot security 반영

--- SpringBootExam/src/main/java/com/nettoall/springboot/web/IndexController.java
+++ SpringBootExam/src/main/java/com/nettoall/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.nettoall.springboot.web;
 
+import com.nettoall.springboot.config.auth.dto.SessionUser;
 import com.nettoall.springboot.service.PostsService;
 import com.nettoall.springboot.web.dto.PostsResponseDto;
 import com.nettoall.springboot.web.dto.PostsUpdateRequestDto;
@@ -11,15 +12,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts",postsService.findAllDesc());
+
+        SessionUser user = (SessionUser)  httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
 
         return "index";
     }
